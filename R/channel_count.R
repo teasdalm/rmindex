a163b1e11edae4ab83bf4cfc262bb264c5a0a364
Matthew Teasdale <mattdt85@gmail.com>
2015-04-15 09:52:06 +0100

    update to remove table command

--- R/channel_count.R
+++ R/channel_count.R
@@ -9,34 +9,15 @@
 
 channel_count <- function(x){
 
-  counts = table(x, exclude=NULL)
-
-  if("A" %in% names(counts)){
-    a_count <- counts["A"]
-  } else{
-    a_count <- 0
+  for(each in x){
+    if(each == "A" | each == "C"){
+      red_channel <- red_channel + 1
+    } else if(each == "G" | each == "T"){
+      green_channel <- green_channel + 1
+    } else{
+      stop("Error: Base not recognised")
+    }
   }
-
-  if("C" %in% names(counts)){
-    c_count <- counts["C"]
-  } else{
-    c_count <- 0
-  }
-
-  if("G" %in% names(counts)){
-    g_count <- counts["G"]
-  } else{
-    g_count <- 0
-  }
-
-  if("T" %in% names(counts)){
-    t_count <- counts["T"]
-  } else{
-    t_count <- 0
-  }
-
-  red_channel <- a_count + c_count
-  green_channel <- g_count + t_count
 
   return(list(red=red_channel,
               green=green_channel))
